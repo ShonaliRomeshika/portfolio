586b05989c44172ec6881140dcb0096dcfc6f0a1
ShonaliRomeshika <shonaliromeshika@gmail.com>
2024-08-21 09:05:19 +0530

    created the backend with sanity

---
+++ backend/schemaTypes/abouts.js
@@ -0,0 +1,27 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'abouts',
+  title: 'Abouts',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'imgUrl',
+      title: 'Image URL',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
