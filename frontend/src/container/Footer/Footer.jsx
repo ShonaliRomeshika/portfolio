7fd0eba59f41e307eb8e01cec645cba64a4dd31e
ShonaliRomeshika <shonaliromeshika@gmail.com>
2024-09-05 00:35:02 +0530

    update the content

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -20,9 +20,9 @@ const Footer = () => {
     e.preventDefault();
     setLoading(true);
 
-    const serviceID = 'service_mge3bpb';
-    const templateID = 'template_33yvern';
-    const userID = 'hGz4IUHnG2fqQnnKH';
+    const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.REACT_APP_EMAILJS_USER_ID;
 
     const templateParams = {
       from_name: name,
@@ -48,11 +48,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">shonaliromeshika@gmail.com</a>
+          <a href="shonaliromeshika@gmail.com" className="p-text">shonaliromeshika@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+94768881337</a>
+          <a href="tel:+94768881337" className="p-text">+94768881337</a>
         </div>
       </div>
 
@@ -93,109 +93,3 @@ export default AppWrap(
   'app__whitebg',
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-
-// import { images } from '../../constants';
-// import { AppWrap, MotionWrap } from '../../wrapper';
-// import { client } from '../../client';
-// import './Footer.scss';
-
-// const Footer = () => {
-//   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-//   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-//   const [loading, setLoading] = useState(false);
-
-//   const { username, email, message } = formData;
-
-//   const handleChangeInput = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const handleSubmit = () => {
-//     setLoading(true);
-
-//     const contact = {
-//       _type: 'contact',
-//       name: formData.username,
-//       email: formData.email,
-//       message: formData.message,
-//     };
-
-//     client.create(contact)
-//       .then(() => {
-//         setLoading(false);
-//         setIsFormSubmitted(true);
-//       })
-//       .catch((err) => console.log(err));
-//   };
-
-//   return (
-//     <>
-//       <h2 className="head-text">Contact me</h2>
-
-//       <div className="app__footer-cards">
-//         <div className="app__footer-card ">
-//           <img src={images.email} alt="email" />
-//           <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
-//         </div>
-//         <div className="app__footer-card">
-//           <img src={images.mobile} alt="phone" />
-//           <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
-//         </div>
-//       </div>
-//       {!isFormSubmitted ? (
-//         <div className="app__footer-form app__flex">
-//           <div className="app__flex">
-//             <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-//           </div>
-//           <div className="app__flex">
-//             <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-//           </div>
-//           <div>
-//             <textarea
-//               className="p-text"
-//               placeholder="Your Message"
-//               value={message}
-//               name="message"
-//               onChange={handleChangeInput}
-//             />
-//           </div>
-//           <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-//         </div>
-//       ) : (
-//         <div>
-//           <h3 className="head-text">
-//             Thank you for getting in touch!
-//           </h3>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default AppWrap(
-//   MotionWrap(Footer, 'app__footer'),
-//   'contact',
-//   'app__whitebg',
-// );
